d1b95f21662306b55994b6f87453b70414da8cb4
Héctor Mauricio de la Rosa Prado <hoftherose@gmail.com>
2021-12-12 14:51:57 -0600

    Update dag

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
-    return 'Hello world from first Airflow DAG!'
+    return 'Hello world from first Airflow DAG from hoftherose!'
 
 dag = DAG('hello_world', description='Hello World DAG',
           schedule_interval='0 12 * * *',
