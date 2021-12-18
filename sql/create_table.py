2402cd3d028b5f30acf47221b2cd0d1342a56648
Héctor de la Rosa <hoftherose@gmail.com>
2021-12-18 14:55:03 -0600

    Create table sql

---
+++ sql/create_table.py
@@ -0,0 +1,15 @@
+"""SQL command definitions"""
+
+CREATE_USER_PURCHASE_TABLE = """
+    DROP TABLE purchase.user_purchase;
+    CREATE TABLE IF NOT EXISTS purchase.user_purchase (
+        invoice_number varchar(10),
+        stock_code varchar(20),
+        detail varchar(1000),
+        quantity int,
+        invoice_date timestamp,
+        unit_price numeric(8,3),
+        customer_id int,
+        country varchar(20)
+    );
+"""
