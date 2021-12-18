f8e2cf5afaffd89b50d4105ebc0a9a8bb870cda6
Héctor de la Rosa <hoftherose@gmail.com>
2021-12-17 23:24:17 -0600

    Print bye

--- hello_world.py
+++ hello_world.py
@@ -2,13 +2,22 @@
 
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 
 def print_hello():
     """Print hello and leave :V"""
-    return 'Hello world from first Airflow DAG from hoftherose version 2!'
+    return 'Hello world from first Airflow DAG from hoftherose!'
+
+
+def print_bye():
+    """Print hello and leave :V"""
+    return 'Good bye world from first Airflow DAG from hoftherose!'
+
+
+dag = DAG('bye_world', description='Bye World DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
 
 
 dag = DAG('hello_world', description='Hello World DAG',
@@ -18,4 +27,7 @@ dag = DAG('hello_world', description='Hello World DAG',
 hello_operator = PythonOperator(
     task_id='hello_task', python_callable=print_hello, dag=dag)
 
-hello_operator
+bye_operator = PythonOperator(
+    task_id='bye_task', python_callable=print_bye(), dag=dag)
+
+hello_operator >> bye_operator
