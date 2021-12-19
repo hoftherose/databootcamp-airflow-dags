f967ebea89d26cc0a86ddf509a4848c0be4f39f0
Héctor de la Rosa <hoftherose@gmail.com>
2021-12-19 00:40:32 -0600

    Reset to 1000 commit size

--- custom/hooks.py
+++ custom/hooks.py
@@ -104,7 +104,7 @@ class GCSToPostgresTransfer(BaseOperator):
             table=f"{self.schema}.{self.table}",
             rows=insert_data,
             target_fields=self.COLUMN_MAPPER.values(),
-            commit_every=1,
+            commit_every=1000,
             replace=False,
         )
         self.log.info("uploaded dataframe to database")
