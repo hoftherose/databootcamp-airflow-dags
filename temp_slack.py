fcb6ecb336c1703fd058ebc1cd1e9eb686ed539f
Héctor de la Rosa <hoftherose@gmail.com>
2021-12-19 11:39:38 -0600

    Swap to webhook

--- temp_slack.py
+++ temp_slack.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # pylint: disable=import-error
 from airflow import DAG
 from airflow.hooks.base_hook import BaseHook
-from airflow.providers.slack.operators.slack import SlackAPIPostOperator
+from airflow.contrib.slack_webhook_operator import SlackWebHookOperator
 
 MESSAGE = "Module2: Challenge yourself - Integrate Slack with Airflow (message sent by Hector)"
 
@@ -16,10 +16,10 @@ with DAG(
     start_date=datetime(2021, 11, 20),
     catchup=False,
 ) as dag:
-    slack_alert = SlackAPIPostOperator(
+    slack_alert = SlackWebHookOperator(
         task_id="slack_msg",
+        http_conn_id="Slack Connection",
         token=BaseHook.get_connection("Slack Connection").password,
-        text=MESSAGE,
-        channel="#data-engineering-bootcamp",
+        message=MESSAGE,
         dag=dag,
     )
