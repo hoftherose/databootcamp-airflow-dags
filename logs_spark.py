d1286df18825b3f72d982abf7f8ff3b012efb69a
Héctor de la Rosa <hoftherose@gmail.com>
2022-01-16 01:04:30 -0600

    Change cluster creation operator

--- logs_spark.py
+++ logs_spark.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.dataproc import (
-    DataprocCreateClusterOperator,
+    ClusterGenerator,
 )
 from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
 from airflow.providers.google.cloud.operators.dataproc import (
@@ -49,14 +49,22 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    create_cluster = DataprocCreateClusterOperator(
+    create_cluster = ClusterGenerator(
         task_id="init_dataproc",
         project_id=PROJECT_ID,
-        cluster_config=CLUSTER_CONFIG,
-        region=REGION,
         cluster_name=CLUSTER_NAME,
+        num_masters=1,
+        num_workers=0,
+        master_machine_type="n1-standard-2",
+        master_disk_type="pd-standard",
+        master_disk_size=30,
+        region=REGION,
         gcp_conn_id="GCP Connection",
-        use_if_exists=True,
+        optional_components=["ANACONDA", "JUPYTER"],
+        properties={"enable_http_port_access": True}
+        # image_version="1.4-debian10",
+        # cluster_config=CLUSTER_CONFIG,
+        # use_if_exists=True,
     )
 
     submit_job = DataprocSubmitJobOperator(
