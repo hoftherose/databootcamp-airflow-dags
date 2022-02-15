7e5e6cbec43b0486d50aba6f15ee119af635f064
Hector de la Rosa <hector.delarosa@wizeline.com>
2022-02-15 13:37:39 -0600

    Separate Variable

--- beam/beam_purchases.py
+++ beam/beam_purchases.py
@@ -4,8 +4,10 @@ from apache_beam.io.jdbc import ReadFromJdbc
 from apache_beam.io.textio import WriteToText
 from apache_beam.coders import registry, RowCoder
 
+DB_IP = "34.83.247.196"
+DB_USER = "db_user"
+DB_PASSWORD = "dbpassword"
 GCS_OUTPUT = "gs://staging_data_layer/purchases"
-
 
 Purchase = NamedTuple("Purchase", [("item_name", float), ("price", float)])
 registry.register_coder(Purchase, RowCoder)
@@ -15,9 +17,9 @@ with Pipeline() as p:
     result = p | "Read from jdbc" >> ReadFromJdbc(
         table_name="user_purchase",
         driver_class_name="org.postgresql.Driver",
-        jdbc_url="jdbc:postgresql://34.127.48.25:5432/purchase",
-        username="db_user",
-        password="dbpassword",
+        jdbc_url=f"jdbc:postgresql://{DB_IP}:5432/purchase",
+        username=DB_USER,
+        password=DB_PASSWORD,
     )
 
     result | "Write" >> WriteToText(GCS_OUTPUT)
